0c740d17359f501cd583164cfea44fb7027796f9
Meriac Michal <michal.meriac@tieto.com>
2020-01-16 08:19:59 +0100

    Updates

---
+++ precache-manifest.f1831e4d3ceea77ffe7aac039d2210c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a95b8f9a7317b0dbf9322535fceb1dc",
+    "url": "/omdb-frontend/index.html"
+  },
+  {
+    "revision": "b18a9461dd8de9acd049",
+    "url": "/omdb-frontend/static/css/main.fb37b9f4.chunk.css"
+  },
+  {
+    "revision": "f38b67436efa86457411",
+    "url": "/omdb-frontend/static/js/2.5f346377.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/omdb-frontend/static/js/2.5f346377.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b18a9461dd8de9acd049",
+    "url": "/omdb-frontend/static/js/main.0fce4d63.chunk.js"
+  },
+  {
+    "revision": "6c9f037caa159edcb7b1",
+    "url": "/omdb-frontend/static/js/runtime-main.d051c915.js"
+  }
+]);
